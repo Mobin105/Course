15d04347f76a5970a5d9f1baf7e613065338818e
Mobin105 <gunfire.exe98@gmail.com>
2024-09-14 18:41:08 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Video_8_2_ورودی_کاربر_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Your name is : {name}");
+
+
+
+            Console.WriteLine("Enter your age ");
+            var age = Console.ReadLine();
+            Console.WriteLine($"Your age is {age}");
+            
+            
+            
+        }
+    }
+}
